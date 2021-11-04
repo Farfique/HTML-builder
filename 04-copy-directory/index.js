c22c160965ff3034e90d50f01861ca114cc6028c
Anna Mikhailova <petunova@gmail.com>
2021-11-04 10:54:09 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fsPromise = require('fs/promises');
+const path = require('path');
+
+let newDirPath = path.join(__dirname, 'files-copy');
+let srcDirPath = path.join(__dirname, 'files');
+
+fsPromise.mkdir(newDirPath, {recursive : true}).then(() => {
+
+    return fsPromise.readdir(newDirPath);
+
+}).then(existingFiles => {
+
+    return Promise.all(existingFiles.map((fileName) => {
+        let pathToFile = path.resolve(newDirPath, fileName);
+        return fsPromise.rm(pathToFile, {force: true});
+    }))
+
+}).then(() => {
+    return fsPromise.readdir(srcDirPath);
+}).then((fileNames) => {
+    for (const fileName of fileNames) {
+        let oldPathToFile = path.resolve(srcDirPath, fileName);
+        let newPathToFile = path.resolve(newDirPath, fileName);
+        fsPromise.copyFile(oldPathToFile, newPathToFile);
+    }
+}).catch(err => console.log(err.message));
+
+
 
